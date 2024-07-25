56a9e19d55c95118ba35a0e24295a350e3e3bf64
Neezie <154235122+neezie1@users.noreply.github.com>
2024-07-25 02:54:58 +0100

    Create unmain.lua

---
+++ unmain.lua
@@ -0,0 +1,47 @@
+getgenv().Config = {
+    Custom_Items = { -- CUSTOM ITEMS
+        { Class = "Lootbox", Item = "Global Event Gift", Price = 800000 },
+        { Class = "Misc", Item = "Mini Pinata", Price = 25000 },
+    },
+    Set_Items = { -- PRICE THRESHOLDS
+        Any_Titanic_Under = 5000000000,
+        Any_Huge_Under = 10000,
+        Any_Exclusive_Under = 10000,
+        Any_Egg_Under = 1,
+    },
+    Rap = { -- RAP SETTINGS
+        { Item = "Any", Rap = "-50", Max_Total_Price = 1, Max_Each_Price = 1, MinRap = 1 },
+        { Item = "Titanic", Rap = "-5", MaxPrice = 5000000000 },
+        { Item = "Huge", Rap = "-10", MaxPrice = 1 },
+        { Item = "Exclusive", Rap = "-15", MaxPrice = 1 },
+        { Item = "Egg", Rap = "-10", MaxPrice = 1 },
+    },
+    ServerHop = { -- SERVERHOP SETTINGS
+        Method = "Terminal", -- [ Terminal | Normal ]
+        Alts = {"",""}, -- List of alt accounts for server hopping
+
+        Item = "Tech Key", -- [ ITEM ID | Random_Huge | Random_Titanic ]
+        Class = "Misc", --[ ITEM CLASS ]
+        Version = nil, --[ nil | 1 | 2 ]
+        Shiny = nil, --[ nil | true ]
+        Tier = nil, --[ Enchants | Potions ]
+    },
+    Webhook = { -- WEBHOOK SETTINGS
+        Url = "https://discord.com/api/webhooks/1265647278291419181/i3Zl8_vBAtABf7pZOigkY78ETfz0jRkoiYyXP5-VQUpUv5wPJuPgAfB_nKcNoXKQ_MHN",
+        User_Method = "DisplayName", -- [ DisplayName | UserName ]
+        Hide_Username = true,
+    },
+    Scripts = { -- SCRIPT SETTINGS
+        Auction = {
+            Auction_Sniper = false, -- Use Auction Sniper
+            View_Board = true, -- Camera facing Auction Board
+            Wait_Out_Listings = false, -- Waits Out Listings
+        },
+        Booth = {
+            Booth_Sniper = true, -- Use Booth Sniper
+            Teleport = false, -- Teleports to Booth
+        },
+    },
+}
+script_key="aVadlcPMMNCDxMyXfuakbZYQGaslMvRf";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/48b8dbab05a3a167c5e72ca8e39d80ed.lua"))()
